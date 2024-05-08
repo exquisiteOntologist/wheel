c84dca73bf5384bcc9f2678ffffc5def49b18792
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-05-08 16:01:35 +0800

    Camera add adjust_camera_speed function

--- src/camera.rs
+++ src/camera.rs
@@ -14,41 +14,7 @@ pub fn move_camera(
     let (_char, t_char) = q_char.single_mut();
     let (_cam, mut t_cam) = q_cam.single_mut();
 
-    let distance_x = t_char.translation.x - t_cam.translation.x;
-    let distance_z = t_char.translation.z - t_cam.translation.z;
-    let camera_should_move_x = distance_x > MAX_CAM_DISTANCE || distance_x < -MAX_CAM_DISTANCE;
-    let camera_should_move_z = distance_z > MAX_CAM_DISTANCE || distance_z < -MAX_CAM_DISTANCE;
-    let m_x = if distance_x > 0. { 1. } else { -1. };
-    let m_z = if distance_z > 0. { 1. } else { -1. };
-
-    // println!("Distance X {:?}", distance_x);
-    // println!("Move camera? {:?}", camera_should_move_x);
-
-    if camera_should_move_x {
-        game.camera.speed_x += (FORWARD_SPEED * 2.) * m_x;
-    }
-
-    if camera_should_move_z {
-        game.camera.speed_z += FORWARD_SPEED * m_z;
-    };
-
-    if game.camera.speed_x != 0. {
-        let dir_m = (m_x);
-        game.camera.speed_x -= FORWARD_SPEED * (game.camera.speed_x / MAX_SPEED) * 0.5 * dir_m;
-    }
-
-    if !(game.camera.speed_x > 0.0001 || game.camera.speed_x < -0.001) {
-        game.camera.speed_x = 0.;
-    }
-
-    if game.camera.speed_z != 0. {
-        let dir_m = (m_z);
-        game.camera.speed_z += FORWARD_SPEED * (game.camera.speed_z / MAX_SPEED) * 0.5 * dir_m;
-    }
-
-    if !(game.camera.speed_z > 0.001 || game.camera.speed_z < -0.001) {
-        game.camera.speed_z = 0.;
-    }
+    adjust_camera_speed(&t_cam, &t_char, &mut game);
 
     let distance = t_cam.translation.distance(t_char.translation);
     let d = distance.max(game.camera.speed_z);
@@ -123,6 +89,44 @@ pub fn move_camera(
         "cam rot (after) X [{:1}] Y [{:2}] Z [{:3}]",
         t_cam.rotation.x, t_cam.rotation.y, t_cam.rotation.z
     );
+}
+
+fn adjust_camera_speed(t_cam: &Transform, t_char: &Transform, game: &mut ResMut<Game>) {
+    let distance_x = t_char.translation.x - t_cam.translation.x;
+    let distance_z = t_char.translation.z - t_cam.translation.z;
+    let camera_should_move_x = distance_x > MAX_CAM_DISTANCE || distance_x < -MAX_CAM_DISTANCE;
+    let camera_should_move_z = distance_z > MAX_CAM_DISTANCE || distance_z < -MAX_CAM_DISTANCE;
+    let m_x = if distance_x > 0. { 1. } else { -1. };
+    let m_z = if distance_z > 0. { 1. } else { -1. };
+
+    // println!("Distance X {:?}", distance_x);
+    // println!("Move camera? {:?}", camera_should_move_x);
+
+    if camera_should_move_x {
+        game.camera.speed_x += (FORWARD_SPEED * 2.) * m_x;
+    }
+
+    if camera_should_move_z {
+        game.camera.speed_z += FORWARD_SPEED * m_z;
+    };
+
+    if game.camera.speed_x != 0. {
+        let dir_m = (m_x);
+        game.camera.speed_x -= FORWARD_SPEED * (game.camera.speed_x / MAX_SPEED) * 0.5 * dir_m;
+    }
+
+    if !(game.camera.speed_x > 0.0001 || game.camera.speed_x < -0.001) {
+        game.camera.speed_x = 0.;
+    }
+
+    if game.camera.speed_z != 0. {
+        let dir_m = (m_z);
+        game.camera.speed_z += FORWARD_SPEED * (game.camera.speed_z / MAX_SPEED) * 0.5 * dir_m;
+    }
+
+    if !(game.camera.speed_z > 0.001 || game.camera.speed_z < -0.001) {
+        game.camera.speed_z = 0.;
+    }
 }
 
 fn get_tran_behind_char(
